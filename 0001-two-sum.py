ba303b307bdcc7db2917137449c9b006446f734f
dante <arham59ru@gmail.com>
2023-05-08 15:15:35 +0300

    Two sum

---
+++ 0001-two-sum.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        hashmap = {}
+        for i, v in enumerate(nums):
+            if (target - v) in hashmap:
+                return [hashmap[target - v], i]
+            hashmap[v] = i  
+'''
+Юзаем хешмап, через enumerate итерируемся и если target - число в хешмапе, то ретернем
+hashmap с ключом taget - v и i, иначе в hashmap добавляем ключ - число, value ключа это индекс 
+'''

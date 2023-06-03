62ec7897c2e49028fc971a6dc10620156f069e51
dante <arham59ru@gmail.com>
2023-06-04 00:04:47 +0300

    +2 tasks

---
+++ two_pointers/0011-container-with-most-water.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxArea(self, height: list[int]) -> int:
+        ans = 0
+        l, r = 0, len(height) - 1 
+        while l < r: 
+            area = min(height[l], height[r]) * (r - l)
+            ans = max(area, ans)
+            if height[l] < height[r]:
+                l += 1 
+            else:
+                r -= 1 
+        return ans 
+    
+'''
+https://leetcode.com/problems/container-with-most-water/description/
+--------------------------------------------------------------------
+
+'''
---
+++ two_pointers/0015-3sum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def threeSum(self, nums: list[int]) -> list[list[int]]:
+        nums.sort()
+        ans = set()
+        for i in range(len(nums)):
+            l = i + 1 
+            r = len(nums) - 1 
+            while l < r:
+                curSum = nums[i] + nums[l] + nums[r]
+                if curSum > 0:
+                    r -= 1 
+                elif curSum < 0:
+                    l += 1 
+                else:
+                    ans.add((nums[i], nums[l], nums[r]))
+                    l += 1 
+                    r -= 1 
+        return ans 
+    
+'''
+https://leetcode.com/problems/3sum/description/
+-----------------------------------------------
+
+'''
